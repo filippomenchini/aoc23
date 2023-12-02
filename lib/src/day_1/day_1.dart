61a8e7e01b90e1bee6459f343d7f44650a3523a6
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2023-12-02 19:03:31 +0100

    Day 1 - Part 2

--- lib/src/day_1/day_1.dart
+++ lib/src/day_1/day_1.dart
@@ -1,18 +1,92 @@
 import 'dart:convert';
 import 'dart:io';
 
-Future<int> day1(String inputFilePath) async {
+const numbersInStrings = {
+  'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9',
+};
+
+Future<int> day1Part1(String inputFilePath) async {
   final input = File(inputFilePath);
   final lines = await input
       .openRead()
       .transform(utf8.decoder)
       .transform(LineSplitter())
       .toList();
+
   final result = lines
       .map((line) => line.replaceAll(RegExp(r'[^0-9]'), ""))
       .map((stringNumber) => int.parse(
           "${stringNumber[0]}${stringNumber[stringNumber.length - 1]}"))
       .reduce((value, element) => value += element);
+
+  return result;
+}
+
+Future<int> day1Part2(String inputFilePath) async {
+  final input = File(inputFilePath);
+  final lines = await input
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(LineSplitter())
+      .toList();
+
+  final result = lines
+      .map(stringToNumber)
+      .map((stringNumber) => int.parse(
+          "${stringNumber[0]}${stringNumber[stringNumber.length - 1]}"))
+      .reduce((value, element) => value += element);
+
+  return result;
+}
+
+String stringToNumber(String input) {
+  String result = "";
+  String currentNumber = "";
+  for (var rune in input.runes) {
+    String char = String.fromCharCode(rune);
+    if (int.tryParse(char) != null) {
+      result += char;
+      currentNumber = "";
+      continue;
+    }
+    currentNumber += char;
+    if (currentNumber.contains("one")) {
+      result += "1";
+      currentNumber = char;
+    } else if (currentNumber.contains("two")) {
+      result += "2";
+      currentNumber = char;
+    } else if (currentNumber.contains("three")) {
+      result += "3";
+      currentNumber = char;
+    } else if (currentNumber.contains("four")) {
+      result += "4";
+      currentNumber = char;
+    } else if (currentNumber.contains("five")) {
+      result += "5";
+      currentNumber = char;
+    } else if (currentNumber.contains("six")) {
+      result += "6";
+      currentNumber = char;
+    } else if (currentNumber.contains("seven")) {
+      result += "7";
+      currentNumber = char;
+    } else if (currentNumber.contains("eight")) {
+      result += "8";
+      currentNumber = char;
+    } else if (currentNumber.contains("nine")) {
+      result += "9";
+      currentNumber = char;
+    }
+  }
 
   return result;
 }
--- test/src/day_1/day_1_test.dart
+++ test/src/day_1/day_1_test.dart
@@ -2,12 +2,23 @@ import 'package:aoc23/aoc23.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Day 1', () async {
-    // Arrange
-    final inputFilePath = 'test_resources/day_1/input.txt';
+  group('Day 1', () {
+    test('Part 1', () async {
+      // Arrange
+      final inputFilePath = 'test_resources/day_1/input.txt';
 
-    // Act
-    final result = await day1(inputFilePath);
-    print("\tDay 1 result: $result");
+      // Act
+      final result = await day1Part1(inputFilePath);
+      print("\tDay 1 - Part 1 result: $result");
+    });
+
+    test('Part 2', () async {
+      // Arrange
+      final inputFilePath = 'test_resources/day_1/input.txt';
+
+      // Act
+      final result = await day1Part2(inputFilePath);
+      print("\tDay 1 - Part 2 result: $result");
+    });
   });
 }
